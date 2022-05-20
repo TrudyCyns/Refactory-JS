4e3139ca6e050d40ece552d880276ee905bbc19b
Gertrude <gcyn51a@gmail.com>
2022-05-20 14:21:48 +0300

    Commit 3

--- exer.js
+++ exer.js
@@ -1,3 +1,35 @@
-let a=24, b=30;
-const d = a+b;
-console.log(d);
+let fname = "Gertrude";
+console.log(typeof (fname));
+
+let num = 45;
+console.log(typeof (num));
+
+myData = {
+    'fname': 'Gertrude',
+    'age': num
+}
+console.log(typeof (myData));
+
+let no = null;
+console.log(typeof (no));
+
+let un;
+console.log(typeof (un));
+
+console.log("======================");
+
+for (i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log("======================");
+
+function myLoop() {
+    for (let i = 0; i <= 100; i += 10) {
+        console.log(i);
+
+    }
+}
+
+console.log("======================");
+myLoop();
---
+++ mon41.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    let c = a + b;
+    console.log(c);
+}
+add();
+
+function mult(a, b) {
+    let c = a * b;
+    return c;
+}
+mult();
