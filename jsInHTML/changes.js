5972cb317f524cdc1a2adc72411933eb9430494b
Gertrude <gcyn51a@gmail.com>
2022-06-02 09:40:17 +0300

    DOM Changes too.

--- jsInHTML/changes.js
+++ jsInHTML/changes.js
@@ -1,23 +1,29 @@
-const paragraph = document.createElement('p');
-const toDoList = document.querySelector('ul');
-const newToDo = document.createElement('li');
-const otherToDo1 = document.createElement('li');
-const otherToDo2 = document.createElement('li');
-const body = document.querySelector('body');
-const anotherToDo = document.createElement('li');
+function change() {
+    const paragraph = document.createElement("p");
+    const toDoList = document.querySelector("ul");
+    const newToDo = document.createElement("li");
+    const otherToDo1 = document.createElement("li");
+    const otherToDo2 = document.createElement("li");
+    const body = document.querySelector("body");
+    const anotherToDo = document.createElement("li");
 
+    paragraph.style.border = "3px solid red";
+    paragraph.innerHTML = "I am a paragraph with <strong>Bold</strong> text.";
 
-paragraph.style.border = '3px solid red';
-paragraph.innerHTML = 'I am a paragraph with <strong>Bold</strong> text.';
+    body.appendChild(paragraph);
 
-body.appendChild(paragraph)
+    newToDo.textContent = "Do homework.";
+    otherToDo1.textContent = "Fetch Water";
+    otherToDo2.textContent = "Mop house.";
+    anotherToDo.textContent = "Pay bills.";
 
-newToDo.textContent = 'Do homework.';
-otherToDo1.textContent = 'Fetch Water';
-otherToDo2.textContent = 'Mop house.';
-anotherToDo.textContent = 'Pay bills.';
+    toDoList.appendChild(newToDo);
+    toDoList.appendChild(otherToDo1);
+    toDoList.appendChild(otherToDo2);
+    toDoList.insertBefore(anotherToDo, toDoList.firstElementChild);
+    toDoList.replaceChild(otherToDo1, toDoList.children[1]);
+    toDoList.removeChild(toDoList.lastElementChild);
+    toDoList.children[2].remove();
+}
 
-toDoList.appendChild(newToDo);
-toDoList.appendChild(otherToDo1);
-toDoList.appendChild(otherToDo2);
-toDoList.insertBefore(anotherToDo, toDoList.firstElementChild);
+change();
