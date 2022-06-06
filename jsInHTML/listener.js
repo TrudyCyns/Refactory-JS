62e66330785a86b7fa6fe703865cf15337690be4
Gertrude <gcyn51a@gmail.com>
2022-06-06 08:07:50 +0300

    new

--- jsInHTML/listener.js
+++ jsInHTML/listener.js
@@ -1,8 +1,8 @@
 const changeText = () => {
-  const p = document.querySelector("p");
-  p.textContent = "I changed because of an event listener.";
+  const p = document.querySelector('p');
+  p.textContent = 'I changed because of an event listener.';
 };
 
 // Listening for click event.
-const btn = document.querySelector("btn");
-btn.addEventListener("click", changeText);
+const btn = document.querySelector('btn');
+btn.addEventListener('click', changeText);
