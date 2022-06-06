7ec63cf987d76530c9a8964e041a77b969bab769
Gertrude <gcyn51a@gmail.com>
2022-06-06 12:20:17 +0300

    Done

--- jsInHTML/listener.js
+++ jsInHTML/listener.js
@@ -6,4 +6,5 @@ const changeText = () => {
 
 // Listening for click event.
 const btn = document.querySelector('button');
+// Dont add brackets after function name because it is being used as an argument in another function (function called = a callback)
 btn.addEventListener('click', changeText);
