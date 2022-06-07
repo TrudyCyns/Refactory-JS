682bd27c7c6fd8a342bd86639fe3f624b6f0b690
Gertrude <gcyn51a@gmail.com>
2022-06-07 12:42:35 +0300

    Validation...

--- jsInHTML/validation.js
+++ jsInHTML/validation.js
@@ -2,3 +2,14 @@ const firstFocus = () => {
   let uId = document.registration.userId.focus();
   return true;
 };
+
+// Validating user ID field.
+const idVal = (min, max) => {
+  let uId = document.registration.userId;
+  let uIdLen = uId.value.length;
+  if (uIdLen == 0 || uIdLen < min || uIdLen >= max) {
+    uId.focus();
+    uId.style.border = '5px solid red';
+    return false;
+  }
+};
